68e1985250972b8a0c73badd5d3230b0006ca4f9
root <root@localhost.localdomain>
2017-07-11 00:52:26 -0400

    first commit

---
+++ features/google_search.feature
@@ -0,0 +1,7 @@
+Feature: Search for Cucumber in Google
+
+Scenario: Search for cucumber wiki page
+ Given I am on google UK
+ And I search for cucumber
+ Then I Should be able to access the cucumber wiki page
+
---
+++ features/support/env.rb
@@ -0,0 +1,12 @@
+require 'capybara'
+require 'cucumber'
+require 'selenium-webdriver'
+require 'capybara/dsl'
+ 
+Capybara.default_driver = :selenium
+Capybara.app_host = "http://www.google.co.uk"
+Capybara.register_driver :selenium do |app|
+  Capybara::Selenium::Driver.new app, browser: :chrome
+end
+  
+World(Capybara::DSL)
